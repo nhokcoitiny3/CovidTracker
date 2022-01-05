980ad70ccc41544b01970770854c619ab161b9ea
Bé Dev Cute <lehonghuan1997@gmail.com>
2022-01-05 08:27:01 +0700

    dev home

--- app/src/main/java/com/tiny/covidtracker/ui/fragment/HealthFragment.kt
+++ app/src/main/java/com/tiny/covidtracker/ui/fragment/HealthFragment.kt
@@ -8,7 +8,11 @@ import androidx.fragment.app.Fragment
 import com.tiny.covidtracker.databinding.FragmentDeclarationBinding
 import com.tiny.covidtracker.databinding.FragmentHealthBinding
 import com.tiny.covidtracker.ui.activities.main.MainViewModel
+import com.tiny.covidtracker.ui.utils.setSafeOnClickListener
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import android.content.Intent
+import android.net.Uri
+
 
 class HealthFragment : Fragment() {
     val viewModel: MainViewModel by sharedViewModel()
@@ -32,5 +36,10 @@ class HealthFragment : Fragment() {
     }
 
     private fun initView() {
+        binding.tvPhone1.setSafeOnClickListener {
+            val number = "tel:" + binding.tvPhone1.text.toString().trim()
+            val callIntent = Intent(Intent.ACTION_CALL, Uri.parse(number))
+            startActivity(callIntent)
+        }
     }
 }
